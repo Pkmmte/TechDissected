bbe0be20327f84e235e267a3ba6ac42bb0f733d9
Pkmmte <pkx7225@gmail.com>
2014-07-27 21:00:40 -0700

    Bye bye, ViewPager...
    Unfortunately, after many painful attempts, I wasn't able to get the ViewPager to work correctly... :(
    It would interfere with the Fading Action Bar. Even with the fading action bar disabled, it messed up the scrolling position for all fragments. I guess my only option was to remove it. :/

--- app/src/main/java/com/pkmmte/techdissected/activity/ArticleActivity.java
+++ app/src/main/java/com/pkmmte/techdissected/activity/ArticleActivity.java
@@ -13,26 +13,24 @@ import com.pkmmte.techdissected.util.RSSManager;
 import java.util.List;
 
 public class ArticleActivity extends FragmentActivity {
-	// ViewPager & Adapter
-	private ViewPager mPager;
-	private ArticlePagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_article);
 
-	    mPager = (ViewPager) findViewById(R.id.articlePager);
+	    Article currentArticle = null;
 	    List<Article> articleList = RSSManager.with(this).get(getIntent().getStringExtra(Constants.KEY_FEED_URL));
-	    mAdapter = new ArticlePagerAdapter(getSupportFragmentManager(), articleList);
-	    mPager.setAdapter(mAdapter);
 
 	    int id = getIntent().getIntExtra(Constants.KEY_ARTICLE_ID, -1);
 	    for(Article article : articleList) {
 			if(article.getId() == id) {
-				mPager.setCurrentItem(articleList.indexOf(article));
+				currentArticle = article;
 				break;
 			}
 	    }
+
+	    FragmentManager fragmentManager = getSupportFragmentManager();
+	    fragmentManager.beginTransaction().replace(R.id.articleContent, ArticleFragment.newInstance(currentArticle)).commit();
     }
 }
